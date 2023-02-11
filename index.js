d2f6cafa25bbb0141edb12d11b58bb6cc945e21c
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-02-12 02:38:55 +0530

    removed static resources

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 const express = require('express');
 
 const app = express();
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     const fs2=require("fs");
