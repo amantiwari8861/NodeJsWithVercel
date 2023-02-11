04dc4572af853568381d4a8034ba27a7eba8c85e
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-02-12 00:03:52 +0530

    added express codes

--- index.js
+++ index.js
@@ -5,7 +5,23 @@
 
 
 //do not use "type": "module" in package.json while using require
-const fs2=require("fs");
-const data=require("./JSquestions.json");
-console.log(fs2);
-console.log(data);
+// const fs2=require("fs");
+// const data=require("./JSquestions.json");
+// console.log(fs2);
+// console.log(data);
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Home Page Route'));
+
+app.get('/about', (req, res) => res.send('About Page Route'));
+
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
