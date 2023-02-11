78acd5f320ee3576e314385d9b7f37280463adbe
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-02-12 00:34:40 +0530

    test fs

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 
 
 //do not use "type": "module" in package.json while using require
-// const fs2=require("fs");
-// const data=require("./JSquestions.json");
-// console.log(fs2);
-// console.log(data);
+const fs2=require("fs");
+const data=require("./JSquestions.json");
+console.log(fs2);
+console.log(data);
 
 const express = require('express');
 
